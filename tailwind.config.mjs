4caa32e1c2f5dba41b68a2c852e68dc9c33da4e6
tyecode <sengphachanh.dev@gmail.com>
2024-06-30 21:07:28 +0700

    chore: Update new design

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,26 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: "1rem",
+        screens: {
+          sm: "100%",
+          md: "768px",
+        },
+      },
+      fontFamily: {
+        "noto-sans": "var(--font-noto-sans)",
+        inter: "var(--font-inter)",
+      },
+      colors: {
+        foreground: "var(--foreground-color)",
+        accent: "var(--accent-color)",
+        white: "var(--white-color)",
+        background: "var(--background-color)",
+      },
+    },
   },
   plugins: [],
 };
